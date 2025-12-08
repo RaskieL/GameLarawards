e7d1f2b8e10574b50d563334a32876483f745603
RaskieL <maiken.dangelo@gmail.com>
2025-12-08 14:47:38 +0100

    created Categories

---
+++ app/Models/Category.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+
+class Category extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $table = "categories";
+
+    /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    use HasFactory, Notifiable;
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -20,6 +21,17 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        Category::factory()->create([
+            [
+                'name' => 'Game of the year',
+                'description' => 'Users most beloved game of the year',
+            ],
+            [
+                'name'=> 'Not the game of the year',
+                'description'=> 'Users absolutely not favourite game of the year',
+            ]
         ]);
     }
 }
