96048ad824d3334063f4f18511f4a0e8b4b89377
xgousset <145650582+xgousset@users.noreply.github.com>
2025-12-16 11:36:09 +0100

    button working

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,10 +10,9 @@
                     {{ __('Manage Games') }}
                 </a>
 
-                <button x-data="" x-on:click="$dispatch('open-modal', 'create-category-modal')" 
-                    class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 focus:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                <a href="{{ route('categories.create') }}" class="inline-flex items-center px-4 py-2 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150">
                     <span class="mr-1 text-base">+</span> {{ __('Add Category') }}
-                </button>
+                </a>
             </div>
         </div>
     </x-slot>
